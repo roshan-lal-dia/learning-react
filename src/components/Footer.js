8cc0ba9e731b70e072357731524fe1b3a06056b3
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-05-13 09:52:48 +0000

    added styles to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
+import "../styles.css";
+
 export default function Footer() {
+  //logic here
   return (
     <div className="Footer">
       <h2>Footer</h2>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import Footer from "./components/Footer";
-
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
