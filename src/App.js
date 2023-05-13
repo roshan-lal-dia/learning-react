4e5c13b3f10ea2368f2751ff1e826899c8f29b9f
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-05-13 09:20:47 +0000

    testing browser preview and hot-reload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Started Editing and already seen the Magic!</h2>
+      <h3>Now trying Hot Reload</h3>
     </div>
   );
 }
