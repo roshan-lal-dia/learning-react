31db2ef1c3694f246d487ca23d95d801f40ac33e
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-05-15 16:46:21 +0000

    lot of messing arounf getting the html into jsx

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import Footer from "./components/Footer";
+import OtpFetch from "./components/otp-fetch";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
@@ -10,5 +11,6 @@ root.render(
   <StrictMode>
     <App />
     <Footer />
+    <OtpFetch />
   </StrictMode>
 );
